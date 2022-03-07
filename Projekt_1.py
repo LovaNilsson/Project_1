77eafe860b818c8f4e3028647af4581172ad71bd
Lova <lova.nilsson@student.tabyenskilda.se>
2022-03-07 14:22:37 +0100

    boten och lite annat

--- Projekt_1.py
+++ Projekt_1.py
@@ -3,18 +3,12 @@
 import os
 import urllib.request
 import json
+import discord
 
 
-text = ""
+def coordinates(plats, landsnummer):
 
-def time(text):
-    print("HEJ")
-
-
-
-def coordinates(stad, landsnummer):
-
-    coordinatesUrl = "http://api.openweathermap.org/geo/1.0/direct?q="+stad+","+landsnummer+"&appid=1b584e5164c75e79f09b30ef48428487"
+    coordinatesUrl = "http://api.openweathermap.org/geo/1.0/direct?q="+plats+","+landsnummer+"&appid=1b584e5164c75e79f09b30ef48428487"
     
     a = urllib.request.urlopen(coordinatesUrl) 
     b = a.read()
@@ -57,25 +51,66 @@ def forecast(longitud, latitud, datum_tid):
     return väder, temperatur
 
 
+def svar(prognos):
+    väderlista = ['klar himmel', 'mestadels klar himmel', 'växlande molnighet', 'halvklart', 'molnigt', 'mulet', 'dimmigt', 'lätta regnskurar', 'regnigt', 'häftiga regnskurar', 'åskstorm', 'lätt snöblandat regn', 'snöblandat regn', 'mycket snöblandat regn','lätt snöfall', 'snöfall', 'mycket snö', 'lätt regn', 'regnigt', 'mycket regn', 'åska', 'lätt snöblandat regn', 'snöblandat regn', 'mycket snöblandat regn', 'lätt snöfall', 'snöfall', 'mycket snö']
+
+    väder = väderlista[prognos[0][0]-1]
+    temperatur = str(prognos[1][0])
+
+    return "Det blir " + väder + " och " + temperatur.replace('.',',') + " °C."
+
+
+
+plats = 'Täby'
+#byt ut å, ä, ö om det finns i platsnamnet
+if "å" in plats:
+    plats = plats.replace('å','a')
+elif "ä" in plats:
+    plats = plats.replace('ä','a')
+elif "ö" in plats:
+    plats = plats.replace('ö','a')
+
+
+
+#formatet för tidsgrejen "2022-3-8 14:00:00"
+
+
 
 tid = "2022-03-08T14:00:00Z"
-stad = "Sundsvall"
 landsnummer = "725"
 
-koordinater = coordinates(stad, landsnummer)
+koordinater = coordinates(plats, landsnummer)
 lon = str("{0:0.0f}".format(koordinater[0]))
 lat = str("{0:0.0f}".format(koordinater[1]))
 
 prognos = forecast(lon, lat, tid)
 
-väderlista = ['klar himmel', 'mestadels klar himmel', 'växlande molnighet', 'halvklart', 'molnigt', 'mulet', 'dimmigt', 'lätta regnskurar', 'regnigt', 'häftiga regnskurar', 'åskstorm', 'lätt snöblandat regn', 'snöblandat regn', 'mycket snöblandat regn','lätt snöfall', 'snöfall', 'mycket snö', 'lätt regn', 'regnigt', 'mycket regn', 'åska', 'lätt snöblandat regn', 'snöblandat regn', 'mycket snöblandat regn', 'lätt snöfall', 'snöfall', 'mycket snö']
+print(svar(prognos))
 
-väder = väderlista[prognos[0][0]-1]
-temperatur = str(prognos[1][0])
-
-svar = "Det blir " + väder + " och " + temperatur.replace('.',',') + " °C."
-print(svar)
 
 
 #git config --global user.email "lova.nilsson@student.tabyenskilda.se"
 #git config --global user.name "Lova"
+
+
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+   print('We have logged in as {0.user}'.format(client))
+ 
+@client.event
+async def on_message(message):
+   if message.author == client.user:
+       return
+ 
+   if message.content.startswith('Vädermannen!'):
+       message.autor = a
+       await message.channel.send('Hej! '+str(a))
+   elif 'Hur blir vädret?' in message.content:
+ # Var vill du veta? Jag kan bara ge prognoser i Sverige'
+       await message.channel.send('')
+
+
+client.run('OTQwMjI2MjY0NTU4NjI0Nzc4.YgET8g.aSgzF3gKC0MOVDvQ-WLRhvVowKo')
