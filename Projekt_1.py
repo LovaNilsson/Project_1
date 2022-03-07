7b29d2904c9c2ea2436e8428c6ad098037a12c71
Lova <lova.nilsson@student.tabyenskilda.se>
2022-03-07 13:08:08 +0100

    test

--- Projekt_1.py
+++ Projekt_1.py
@@ -1,29 +1,94 @@
+#Test
 
-import discord
 import os
 import urllib.request
 import json
- 
-client = discord.Client()
-
-@client.event
-async def on_ready():
-   print('We have logged in as {0.user}'.format(client))
- 
-@client.event
-async def on_message(message):
-   if message.author == client.user:
-       return
- 
-   if message.content.startswith('Hur blir vädret?'):
-       await message.channel.send('Var och när menar du?')
-   elif 'Bye' in message.content:
-       await message.channel.send('')
 
 
-       #översätta svensk tid till coordinated universal time
-       #hitta rätt koordinater utifrån vilken plats som väljs
-       #leta i API
+text = ""
 
-client.run('')
+def time(text):
+    print("HEJ")
+
+
+
+def coordinates(stad, landsnummer):
+
+    coordinatesUrl = "http://api.openweathermap.org/geo/1.0/direct?q="+stad+","+landsnummer+"&appid=1b584e5164c75e79f09b30ef48428487"
+    
+    a = urllib.request.urlopen(coordinatesUrl) 
+    b = a.read()
+    c = json.loads(b)
+    lon = c[0]["lon"]
+    lat = c[0]["lat"]
+    
+    return(lon, lat)
+
+
+def forecast(longitud, latitud, datum_tid):
+
+    lon = longitud
+    lat = latitud
+    forecastUrl = 'https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/'+lon+'/lat/'+lat+'/data.json'
+    
+    a = urllib.request.urlopen(forecastUrl) 
+    b = a.read()
+    c = json.loads(b)
+    tidsserie = c["timeSeries"]
+    
+    #Hitta rätt tid och datum
+    d = 0
+    tid = datum_tid
+    while tidsserie[d]["validTime"]!=tid:
+        d+=1
+    
+    #Hitta temperatur
+    e = 0
+    while tidsserie[d]["parameters"][e]["name"]!="t":
+        e+=1
+    temperatur = tidsserie[d]["parameters"][e]["values"]
+
+    #Hitta väder
+    f = 0
+    while tidsserie[d]["parameters"][f]["name"]!="Wsymb2":
+        f+=1
+    väder = tidsserie[d]["parameters"][f]["values"]
+    
+    return väder, temperatur
+
+
+
+tid = "2022-03-08T14:00:00Z"
+stad = "Sundsvall"
+landsnummer = "725"
+
+koordinater = coordinates(stad, landsnummer)
+lon = str("{0:0.0f}".format(koordinater[0]))
+lat = str("{0:0.0f}".format(koordinater[1]))
+
+prognos = forecast(lon, lat, tid)
+
+print(prognos[0])
+
+väder[prgonos[0]-1]
+väder = ['klar himmel', 'mestadels klar himmel', 'växlande molnighet', 'halvklart', 'molnigt', 'mulet', 'dimmigt', 'lätta regnskurar', 'regnigt', 'häftiga regnskurar', 'åskstorm', 'snöblandat regn', ]
+1	
+
+12	Light sleet showers
+13	Moderate sleet showers
+14	Heavy sleet showers
+15	Light snow showers
+16	Moderate snow showers
+17	Heavy snow showers
+18	Light rain
+19	Moderate rain
+20	Heavy rain
+21	Thunder
+22	Light sleet
+23	Moderate sleet
+24	Heavy sleet
+25	Light snowfall
+26	Moderate snowfall
+27	Heavy snowfall
+
 
